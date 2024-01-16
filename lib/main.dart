487bdde51986cfebab756989e06b448c2159bcb4
Alfredo Paes da Luz <alfredo.alfpaes@gmail.com>
2024-01-16 00:16:49 -0300

    Create firs Layout on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    )
-  );
+  runApp(MiCardApp());
 }
 
+class MiCardApp extends StatelessWidget {
+  const MiCardApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+                color: Colors.white,
+                height: 250.0,
+                width: 250.0,
+                child: const Center(
+                  child: Text(
+                    "Hello World!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                        color: Colors.black87),
+                  ),
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
